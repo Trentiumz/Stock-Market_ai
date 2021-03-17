bdeb6cca2a30497f0bbaad908e8f3d0c4b68651c
Daniel Ye <danielyeqt@gmail.com>
2021-03-16 20:02:25 -0400

    Turn to Numpy

--- Attempt_1/training_model.py
+++ Attempt_1/training_model.py
@@ -6,12 +6,13 @@ import os
 DATA_PATH = "./Data/Detailed_Stocks_1/"
 EXAMPLE_BATCH_SIZE = 100
 SHUFFLE_BUFFER_SIZE = 30
-BATCH_SIZE = 10
+BATCH_SIZE = 20
 RNN_UNITS = 1024
 
 files = os.listdir(DATA_PATH)
 
 train_features = []
+train_labels = []
 
 for file in files:
     with open(f"{DATA_PATH}{file}", "rt") as inp:
@@ -22,18 +23,21 @@ for file in files:
         features = features[:EXAMPLE_BATCH_SIZE * int(len(features) / EXAMPLE_BATCH_SIZE)]
         labels = labels[:EXAMPLE_BATCH_SIZE * int(len(labels) / EXAMPLE_BATCH_SIZE)]
 
-        features = np.array(features)
-        mag = np.max(features) - np.min(features)
-        features -= np.min(features)
-        features *= 1/mag
+        train_features += features
+        train_labels += labels
 
-        train_features += [((features[x]), (labels[x])) for x in range(len(features))]
 train_features = np.array(train_features)
-train_features.resize((len(train_features),2,1))
+train_labels = np.array(train_labels)
+train_features.resize((int(len(train_features) / EXAMPLE_BATCH_SIZE), EXAMPLE_BATCH_SIZE))
+train_labels.resize((int(len(train_labels) / EXAMPLE_BATCH_SIZE), EXAMPLE_BATCH_SIZE))
+for i in range(len(train_features)):
+    train_features[i,:] -= np.min(train_features[i,:])
+    train_features[i,:] *= 1/(np.max(train_features[i]) - np.min(train_features[i]))
 
-train_data = tf.data.Dataset.from_tensor_slices(train_features).map(lambda x: (x[0], x[1])).batch(EXAMPLE_BATCH_SIZE, drop_remainder=True)
-train_data = train_data.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
-print(train_data)
+train_features.resize((len(train_features), EXAMPLE_BATCH_SIZE, 1))
+train_labels.resize((len(train_labels), EXAMPLE_BATCH_SIZE, 1))
+
+print(train_features.shape)
 
 model = keras.models.Sequential([
     tf.keras.layers.LSTM(RNN_UNITS, return_sequences=True, stateful=True, recurrent_initializer="glorot_uniform"),
@@ -47,4 +51,5 @@ def loss(labels, logits):
 model.compile(optimizer="adam", loss=keras.losses.binary_crossentropy)
 print(model.summary())
 
-history = model.fit(train_data, epochs=100)
+
+history = model.fit(train_features[:BATCH_SIZE], train_labels[:BATCH_SIZE], epochs=100)
