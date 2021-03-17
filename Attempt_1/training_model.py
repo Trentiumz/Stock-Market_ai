6e0b917e933a2aee63939cfc828e779d5bb35818
Daniel Ye <danielyeqt@gmail.com>
2021-03-16 20:07:34 -0400

    Convert into more assured dataset

--- Attempt_1/training_model.py
+++ Attempt_1/training_model.py
@@ -6,7 +6,7 @@ import os
 DATA_PATH = "./Data/Detailed_Stocks_1/"
 EXAMPLE_BATCH_SIZE = 100
 SHUFFLE_BUFFER_SIZE = 30
-BATCH_SIZE = 20
+BATCH_SIZE = 32
 RNN_UNITS = 1024
 
 files = os.listdir(DATA_PATH)
@@ -28,16 +28,13 @@ for file in files:
 
 train_features = np.array(train_features)
 train_labels = np.array(train_labels)
-train_features.resize((int(len(train_features) / EXAMPLE_BATCH_SIZE), EXAMPLE_BATCH_SIZE))
-train_labels.resize((int(len(train_labels) / EXAMPLE_BATCH_SIZE), EXAMPLE_BATCH_SIZE))
-for i in range(len(train_features)):
-    train_features[i,:] -= np.min(train_features[i,:])
-    train_features[i,:] *= 1/(np.max(train_features[i]) - np.min(train_features[i]))
+train_features.resize((len(train_features), 1))
+train_labels.resize((len(train_labels), 1))
 
-train_features.resize((len(train_features), EXAMPLE_BATCH_SIZE, 1))
-train_labels.resize((len(train_labels), EXAMPLE_BATCH_SIZE, 1))
-
-print(train_features.shape)
+train_data = tf.data.Dataset.from_tensor_slices((train_features, train_labels)).batch(EXAMPLE_BATCH_SIZE,
+                                                                                      drop_remainder=True).shuffle(
+    SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+print(train_data)
 
 model = keras.models.Sequential([
     tf.keras.layers.LSTM(RNN_UNITS, return_sequences=True, stateful=True, recurrent_initializer="glorot_uniform"),
@@ -46,10 +43,7 @@ model = keras.models.Sequential([
 ])
 model.build(input_shape=(BATCH_SIZE, None, 1))
 
-def loss(labels, logits):
-    return keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 model.compile(optimizer="adam", loss=keras.losses.binary_crossentropy)
 print(model.summary())
 
-
-history = model.fit(train_features[:BATCH_SIZE], train_labels[:BATCH_SIZE], epochs=100)
+history = model.fit(train_data, epochs=100)
